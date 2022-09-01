41f69d88d4368ed41ec645ad9fe6678567c303fe
Grace0124 <lyuhan@gmail.com>
2022-09-01 17:39:50 -0400

    Make separate functions for computer's choice and playing round
    Removed getComputerChoice() from the PlayRound() function. Replaced
    console.log() with return in each scenario.

--- javascript.js
+++ javascript.js
@@ -1,66 +1,56 @@
-function playRound() {
-    let computerSelection;
-    //computer chooses rock, paper, or scissors
-    function getComputerChoice() {
-        let x = Math.floor(Math.random()*3);
-        if (x == 0) {
-            computerSelection = 'rock';
-        } else if (x == 1) {
-            computerSelection = 'paper';
-        } else {
-            computerSelection = 'scissors';
-        }
-        return computerSelection;
+//computer chooses rock, paper, or scissors
+function getComputerChoice() {
+    let x = Math.floor(Math.random()*3);
+    if (x == 0) {
+        computerSelection = 'rock';
+    } else if (x == 1) {
+        computerSelection = 'paper';
+    } else {
+        computerSelection = 'scissors';
     }
-    getComputerChoice();
-    console.log(computerSelection);
+    return computerSelection;
+}
 
+
+function playRound(playerSelection, computerSelection) {
     //player selects rock, paper, or scissors
-    playerSelection = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
     if (playerSelection == 'rock'){
         switch(computerSelection) {
             case 'rock':
-                console.log("Tie");
-                break;
+                return "Tie";
             case 'paper':
-                console.log("Lose");
-                break;
+                return "Lose";
             case 'scissors':
-                console.log("Win");
-                break;
+                return "Win";
         }
     }
     if (playerSelection == 'paper'){
         switch(computerSelection) {
             case 'paper':
-                console.log("Tie");
-                break;
+                return "Tie";
             case 'scissors':
-                console.log("Lose");
-                break;
+                return "Lose";
             case 'rock':
-                console.log("Win");
-                break;
+                return "Win";
         }
     }
     if (playerSelection == 'scissors'){
         switch(computerSelection) {
             case 'scissors':
-                console.log("Tie");
-                break;
+                return "Tie";
             case 'rock':
-                console.log("Lose");
-                break;
+                return "Win";
             case 'paper':
-                console.log("Win");
-                break;
+                return "Lose";
         }
     }
 
-
 }
 
-playRound();
+playerSelection = prompt("Choose Rock, Paper, or Scissors").toLowerCase();
+computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
 //declare winner
 
 
